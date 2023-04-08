44d64f8d555d435c0bd9d01aa50e09c328218346
Sheng-Feng <130227191+Sheng-Feng@users.noreply.github.com>
2023-04-08 21:22:07 +0800

    Add files via upload

---
+++ stanCode_projects/anagram_game_solver/anagram.py
@@ -0,0 +1,108 @@
+"""
+File: anagram.py
+Name: Aaron Kao
+----------------------------------
+This program recursively finds all the anagram(s)
+for the word input by user and terminates when the
+input string matches the EXIT constant defined
+at line 19
+
+If you correctly implement this program, you should see the
+number of anagrams for each word listed below:
+    * arm -> 3 anagrams
+    * contains -> 5 anagrams
+    * stop -> 6 anagrams
+    * tesla -> 10 anagrams
+    * spear -> 12 anagrams
+"""
+
+import time                   # This file allows you to calculate the speed of your algorithm
+
+# Constants
+FILE = 'dictionary.txt'       # This is the filename of an English dictionary
+EXIT = '-1'                   # Controls when to stop the loop
+
+
+def main():
+    """
+    This program shows the anagram game, which is a word or phrase formed by rearranging
+    the letters of a different word.
+    """
+    print('Welcome to stanCode "Anagram Generator" (or -1 to quit)')
+    while True:
+        word = input('Find anagrams for: ')
+        if word == EXIT:
+            break
+        else:
+            start = time.time()
+            dic_lst = read_dictionary(word)
+            find_anagrams(word, dic_lst)
+            end = time.time()
+            print('----------------------------------')
+            print(f'The speed of your anagram algorithm: {end-start} seconds.')
+
+
+def read_dictionary(s):
+    """
+    param s: input word
+    Load the dictionary file and Store the word at DIC_LST.
+    """
+    dic_lst = []
+    with open(FILE, 'r') as f:
+        for line in f:
+            # Algorithm optimization1 - Store the same number of input word
+            if len(line.strip()) == len(s):
+                dic_lst.append(line.strip())
+    dic_lst = set(dic_lst)     # Algorithm optimization2 : set {'aah', 'aal'...}   O(1)
+    return dic_lst
+
+
+def find_anagrams(s, dic_lst):
+    """
+    param s: input word
+    return: the anagram words from dictionary.txt
+    """
+    print('Searching...')
+    word_lst = []
+    find_anagrams_helper(s, "", len(s), dic_lst, word_lst)
+    print(str(len(word_lst)) + ' anagrams:', word_lst)
+
+
+def find_anagrams_helper(s, current_s, len_s, dic_lst, word_lst):
+    """
+    param s: input word
+    param current_s: the initial string
+    param len_s: the length of input word
+    return: the anagram words from dictionary.txt
+    """
+    if len(current_s) == len_s and current_s in dic_lst:  # Base Case - Anagram word form dictionary.txt
+        if current_s not in word_lst:       # Avoid storing repeated permutations, ex: apple has two p letters.
+            word_lst.append(current_s)
+            print('Found:', current_s)
+            print('Searching...')
+    else:
+        # Choose
+        for i in range(len(s)):
+            s_remain = s[:i]+s[i+1:]       # Unselected remaining letters.
+            current_s += s[i]              # Pick the letter and store at the string
+        # Explore
+            #   if has_prefix(current_s, dic_lst):   # for loop is O(N)
+            find_anagrams_helper(s_remain, current_s, len_s, dic_lst, word_lst)
+                # Recursion - the next step of searching word.
+        # Un-choose
+            current_s = current_s[:-1]     # Back to previous step.
+
+
+def has_prefix(sub_s, dic_lst):
+    """
+    :param sub_s: the current searching word
+    :return: True/False ,whether the word exists in the DIC_LST
+    """
+    for line in dic_lst:
+        if line.startswith(sub_s):
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    main()
---
+++ stanCode_projects/name_searching_system/babygraphics.py
@@ -0,0 +1,129 @@
+"""
+File: babygraphics.py
+Name: Aaron Kao
+--------------------------------
+SC101 Baby Names Project
+Adapted from Nick Parlante's Baby Names assignment by Jerry Liao.
+"""
+
+import tkinter
+import babynames
+import babygraphicsgui as gui
+
+FILENAMES = [
+    'data/full/baby-1900.txt', 'data/full/baby-1910.txt',
+    'data/full/baby-1920.txt', 'data/full/baby-1930.txt',
+    'data/full/baby-1940.txt', 'data/full/baby-1950.txt',
+    'data/full/baby-1960.txt', 'data/full/baby-1970.txt',
+    'data/full/baby-1980.txt', 'data/full/baby-1990.txt',
+    'data/full/baby-2000.txt', 'data/full/baby-2010.txt'
+]
+CANVAS_WIDTH = 1000
+CANVAS_HEIGHT = 600
+YEARS = [1900, 1910, 1920, 1930, 1940, 1950,
+         1960, 1970, 1980, 1990, 2000, 2010]
+GRAPH_MARGIN_SIZE = 20
+COLORS = ['red', 'purple', 'green', 'blue']
+TEXT_DX = 2
+LINE_WIDTH = 2
+MAX_RANK = 1000
+
+
+def get_x_coordinate(width, year_index):
+    """
+    Given the width of the canvas and the index of the current year
+    in the YEARS list, returns the x coordinate of the vertical
+    line associated with that year.
+
+    Input:
+        width (int): The width of the canvas
+        year_index (int): The index where the current year is in the YEARS list
+    Returns:
+        x_coordinate (int): The x coordinate of the vertical line associated
+                            with the current year.
+    """
+    x_coordinate = GRAPH_MARGIN_SIZE + (width-2*GRAPH_MARGIN_SIZE) / len(YEARS) * year_index
+    return x_coordinate
+
+
+def draw_fixed_lines(canvas):
+    """
+    Draws the fixed background lines on the canvas.
+
+    Input:
+        canvas (Tkinter Canvas): The canvas on which we are drawing.
+    """
+    canvas.delete('all')            # delete all existing lines from the canvas
+
+    # ----- Write your code below this line ----- #
+    # Background Setting.
+    canvas.create_line(GRAPH_MARGIN_SIZE, GRAPH_MARGIN_SIZE, CANVAS_WIDTH-GRAPH_MARGIN_SIZE, GRAPH_MARGIN_SIZE)
+    canvas.create_line(GRAPH_MARGIN_SIZE, CANVAS_HEIGHT-GRAPH_MARGIN_SIZE,
+                       CANVAS_WIDTH-GRAPH_MARGIN_SIZE, CANVAS_HEIGHT-GRAPH_MARGIN_SIZE)
+    for i in range(len(YEARS)):
+        canvas.create_line(get_x_coordinate(CANVAS_WIDTH, i), 0, get_x_coordinate(CANVAS_WIDTH, i), CANVAS_HEIGHT)
+        canvas.create_text(get_x_coordinate(CANVAS_WIDTH, i) + TEXT_DX,
+                           CANVAS_HEIGHT-GRAPH_MARGIN_SIZE, text=YEARS[i], anchor=tkinter.NW)
+
+
+def draw_names(canvas, name_data, lookup_names):
+    """
+    Given a dict of baby name data and a list of name, plots
+    the historical trend of those names onto the canvas.
+
+    Input:
+        canvas (Tkinter Canvas): The canvas on which we are drawing.
+        name_data (dict): Dictionary holding baby name data
+        lookup_names (List[str]): A list of names whose data you want to plot
+
+    Returns:
+        This function does not return any value.
+    """
+    draw_fixed_lines(canvas)        # draw the fixed background grid
+
+    # ----- Write your code below this line ----- #
+    for i in range(len(lookup_names)):          # For Loop - Search for each entered baby name.
+        get_y_coordinate = []                   # Store the y location in every year.
+        for j in range(len(YEARS)):             # For Loop - Run every year of YEARS list.
+            if str(YEARS[j]) in name_data[lookup_names[i]]:  # Show the rank and name at certain location.
+                get_y_coordinate.append(GRAPH_MARGIN_SIZE + (CANVAS_HEIGHT-2*GRAPH_MARGIN_SIZE)
+                                        * int(name_data[lookup_names[i]][str(YEARS[j])]) / MAX_RANK)
+                canvas.create_text(get_x_coordinate(CANVAS_WIDTH, j) + TEXT_DX
+                                   , get_y_coordinate[j]
+                                   , text=lookup_names[i] + ' ' + name_data[lookup_names[i]][str(YEARS[j])]
+                                   , anchor=tkinter.SW, fill=COLORS[i % len(COLORS)])     # Loop the color setting.
+            else:      # If the rank is out of the MAX., replace the rank with an "*".
+                get_y_coordinate.append(CANVAS_HEIGHT-GRAPH_MARGIN_SIZE)
+                canvas.create_text(get_x_coordinate(CANVAS_WIDTH, j) + TEXT_DX
+                                   , get_y_coordinate[j]
+                                   , text=lookup_names[i] + ' *'
+                                   , anchor=tkinter.SW, fill=COLORS[i % len(COLORS)])
+        for j in range(len(YEARS)-1):           # Draw the line between two data points.
+            canvas.create_line(get_x_coordinate(CANVAS_WIDTH, j)
+                               , get_y_coordinate[j],
+                               get_x_coordinate(CANVAS_WIDTH, j+1)
+                               , get_y_coordinate[j+1]
+                               , fill=COLORS[i % len(COLORS)], width=LINE_WIDTH)      # Loop the color setting.
+
+
+# main() code is provided, feel free to read through it but DO NOT MODIFY
+def main():
+    # Load data
+    name_data = babynames.read_files(FILENAMES)
+
+    # Create the window and the canvas
+    top = tkinter.Tk()
+    top.wm_title('Baby Names')
+    canvas = gui.make_gui(top, CANVAS_WIDTH, CANVAS_HEIGHT, name_data, draw_names, babynames.search_names)
+
+    # Call draw_fixed_lines() once at startup so we have the lines
+    # even before the user types anything.
+    draw_fixed_lines(canvas)
+
+    # This line starts the graphical loop that is responsible for
+    # processing user interactions and plotting data
+    top.mainloop()
+
+
+if __name__ == '__main__':
+    main()
